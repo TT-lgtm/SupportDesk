9830ccf2b53a932d3d6e5cf3554badadbc061993
TT-lgtm <comkosmos.sch@gmail.com>
2026-09-04 11:07:01 +0300

    Добавить слой логики и вывод результата

---
+++ SupportDesk.Logic/TicketService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SupportDesk.Data;
+
+namespace SupportDesk.Logic
+{
+    public class TicketService
+    {
+        private readonly TicketRepository _repository = new();
+        public List<Ticket> GetImportant()
+        {
+            return _repository.GetAll().Where(item => item.Priority == "high").ToList();
+        }
+    }
+}
--- SupportDesk/Program.cs
+++ SupportDesk/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SupportDesk.Logic;
 
 namespace SupportDesk
 {
@@ -11,6 +12,12 @@ namespace SupportDesk
         static void Main(string[] args)
         {
             Console.WriteLine("SupportDesk - Олег");
+            var service = new TicketService();
+            Console.WriteLine("Отобранные записи:");
+            foreach (var item in service.GetImportant())
+            {
+                Console.WriteLine($"{item.Id}: {item.Subject}");
+            }
         }
     }
 }
