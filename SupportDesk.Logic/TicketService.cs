4cd4928d46737740038cc3cfb4053f45d3d55224
TT-lgtm <comkosmos.sch@gmail.com>
2026-09-08 15:24:32 +0300

    Ввести контракт хранилища в слое логики

--- SupportDesk.Logic/TicketService.cs
+++ SupportDesk.Logic/TicketService.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SupportDesk.Data;
 
 namespace SupportDesk.Logic
 {
     public class TicketService
     {
-        private readonly TicketRepository _repository = new();
+        private readonly ITicketRepository _repository;
+        public TicketService(ITicketRepository repository)
+        {
+            _repository = repository;
+        }
+
         public List<Ticket> GetImportant()
         {
             return _repository.GetAll().Where(item => item.Priority == "high").ToList();
